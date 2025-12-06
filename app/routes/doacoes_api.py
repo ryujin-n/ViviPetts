2ba6b9a21d5630077b437ffa7fd444cff2b758ee
matheusls261 <matheuslsilveira.2006@gmail.com>
2025-12-06 13:52:42 -0300

    Update doacoes_api.py

--- app/routes/doacoes_api.py
+++ app/routes/doacoes_api.py
@@ -23,6 +23,21 @@ def criar_doacao():
     db.session.commit()
     return jsonify(doacao.to_dict()), 201
 
+@doacoes_bp.route('/<int:id>', methods=['PUT'])
+def atualizar_doacao(id):
+    data = request.get_json() or request.form
+    doacao = Doacao.query.get_or_404(id)
+
+    doacao.doador_id = data.get('doador_id', doacao.doador_id)
+    doacao.tipo_doacao = data.get('tipo_doacao', doacao.tipo_doacao)
+    doacao.valor = data.get('valor', doacao.valor)
+    doacao.data_doacao = data.get('data_doacao', doacao.data_doacao)
+
+    db.session.commit()
+
+    return jsonify(doacao.to_dict())
+
+
 @doacoes_bp.route('/<int:id>', methods=['DELETE'])
 def deletar_doacao(id):
     doacao = Doacao.query.get_or_404(id)
